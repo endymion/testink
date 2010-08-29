beae4ebf5bfcdd41983cfa88482b43fd9e7905a0
Ryan Alyn Porter <rap@endymion.com>
2010-08-28 20:12:21 -0400

    Initial commit to testink.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testink::Application.config.secret_token = 'e1591f3499c5f7b12f7326da20f5ea1cd816df8520a4894eb7814c4cfc81ee464c6a219929e33956930158cff051011f91f66f5f49a87f5edcd89147342d965e'
